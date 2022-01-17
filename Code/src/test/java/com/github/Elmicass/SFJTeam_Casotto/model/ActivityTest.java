af0f1ad4cb93846be72c55840c3607599ed56676
Chitarrina <leon.nowak57@gmail.com>
2022-01-17 21:21:23 +0100

    Starting tests of User.java class
    Tests implemented:
    
    - ID attribute cases
    - name attribute cases
    - surname attribute cases
    - email attribute cases
    -getRole();

--- Code/src/test/java/com/github/Elmicass/SFJTeam_Casotto/model/ActivityTest.java
+++ Code/src/test/java/com/github/Elmicass/SFJTeam_Casotto/model/ActivityTest.java
@@ -1,10 +1,21 @@
 package com.github.Elmicass.SFJTeam_Casotto.model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 public class ActivityTest {
     @Test
-	void contextLoads() {
+
+	/*public void shouldGetID(){
+		Equipment equipment = new Equipment("Test Name","Test Desc","Test Type");
+		Activity activity = new Activity("Test Name","Test Desc", 10, null, null, Set equipment);
+		assertNotEquals(null, activity.getID(), "message");
 	}
+	public void shouldSetName() {
+		//Activity activity = new Activity("Test Name","Test Desc", 10, null, null, null);
+		//activity.setName("foo");
+		assertFalse(true);
+	}*/
 }
--- Code/src/test/java/com/github/Elmicass/SFJTeam_Casotto/model/UserTest.java
+++ Code/src/test/java/com/github/Elmicass/SFJTeam_Casotto/model/UserTest.java
@@ -1,10 +1,92 @@
 package com.github.Elmicass.SFJTeam_Casotto.model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 public class UserTest {
-    @Test
-	void contextLoads() {
+    //tests of ID attribute
+	@Test
+	void shouldGetID(){
+		User user = new User("test name","test surname","test email");
+		assertNotNull(user.getID());
 	}
+
+	//tests of name attribute
+	@Test
+	void shouldGetName(){
+		User user = new User("test name","test surname","test email");
+		assertNotNull(user.getName());
+	}
+	@Test
+	void shouldsetName(){
+		User user = new User("test name","test surname","test email");
+		user.setName("foo");
+		assertEquals("foo",user.getName());
+	}
+	@Test
+	void shouldThrowIAExceptionWhenSetBlankName(){
+		User user = new User("test name","test surname","test email");
+		assertThrows(IllegalArgumentException.class,() -> user.setName(""));
+	}
+	@Test
+	void shouldThrowIAExceptionWhenSetNullName(){
+		User user = new User("test name","test surname","test email");
+		assertThrows(NullPointerException.class,() -> user.setName(null));
+	}
+
+	//tests of surname attribute
+	@Test
+	void shouldGetSurame(){
+		User user = new User("test name","test surname","test email");
+		assertNotNull(user.getSurname());
+	}
+	@Test
+	void shouldsetSurname(){
+		User user = new User("test name","test surname","test email");
+		user.setSurname("foo");
+		assertEquals("foo",user.getSurname());
+	}
+	@Test
+	void shouldThrowIAExceptionWhenSetBlankSurname(){
+		User user = new User("test name","test surname","test email");
+		assertThrows(IllegalArgumentException.class,() -> user.setSurname(""));
+	}
+	@Test
+	void shouldThrowIAExceptionWhenSetNullSurname(){
+		User user = new User("test name","test surname","test email");
+		assertThrows(NullPointerException.class,() -> user.setSurname(null));
+	}
+	
+	//tests of surname attribute
+	@Test
+	void shouldGetEmail(){
+		User user = new User("test name","test surname","test email");
+		assertNotNull(user.getEmail());
+	}
+	@Test
+	void shouldsetEmail(){
+		User user = new User("test name","test surname","test email");
+		user.setEmail("foo");
+		assertEquals("foo",user.getEmail());
+	}
+	@Test
+	void shouldThrowIAExceptionWhenSetBlankEmail(){
+		User user = new User("test name","test surname","test email");
+		assertThrows(IllegalArgumentException.class,() -> user.setEmail(""));
+	}
+	@Test
+	void shouldThrowNPExceptionWhenSetNullEmail(){
+		User user = new User("test name","test surname","test email");
+		assertThrows(NullPointerException.class,() -> user.setEmail(null));
+	}
+	@Test
+
+	//tests of role attribute
+	void shouldGetRole(){
+		User user = new User("test name","test surname","test email");
+		assertNotNull(user.getRole());
+	}
+	
 }
